8f7190d28e90f15bddb784fb18c12a0d61479ba5
marco <marco@mark.mark>
2025-06-11 17:56:18 -0300

    main testing

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,11 @@ import services.notifications.NotificationService;
 import strategies.notification.concretes.*;
 
 public class Main {
+
     public static void main(String[] args) {
         Message welcomeMessage = MessageFactory.createMessage("SIMPLE", "Bem-vindo(a) ao nosso sistema!");
         Message alertMessage = MessageFactory.createMessage("URGENT", "Falha crítica detectada no servidor XYZ. Ação imediata requerida.");
+        Message discountMessage = MessageFactory.createMessage("PROMOTIONAL", "50% DE DESCONTO EM MATERIAIS ESCOLARES! SOMENTE ATÉ DIA DD/MM!");
 
         NotificationService notificationService = new NotificationService();
 
@@ -14,12 +16,13 @@ public class Main {
         notificationService.setStrategy(new EmailNotificationStrategy());
         notificationService.sendNotification(welcomeMessage, "aluno@exemplo.com");
         notificationService.sendNotification(alertMessage, "admin@exemplo.com");
+        notificationService.sendNotification(discountMessage, "alguminfeliz@exemplo.com");
 
         System.out.println("--- Mudando para Estratégia de SMS ---");
         notificationService.setStrategy(new SMSNotificationStrategy());
         notificationService.sendNotification(welcomeMessage, "+5511912345678");
-
         notificationService.sendNotification(alertMessage, "+5521987654321");
+        notificationService.sendNotification(discountMessage, "+5531924680246");
 
     }
 }
